b25100b9636618ea172f5d539f412e30b23b56af
shresthyadav618 <manushresth.official24669@gmail.com>
2023-08-17 20:49:45 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import { supportedLanguages } from "./api/api";
 function App() {
+
+  const [select,changeSelect] = useState(null);
+  
+  useEffect(()=>{
+    console.log(process.env.REACT_APP_API_KEY);
+    async function getData(){
+      const url = supportedLanguages;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+		'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+  console.log(result.languages);
+
+  changeSelect(result.languages)
+
+} catch (error) {
+	console.error(error);
+}
+    }
+
+    getData();
+  },[])
+
+  
+  const options_req = select ? select.map((elm,idx)=>{
+    // if(idx<10)
+    return <option value="${elm.language}">${elm.name}</option>
+  }) : null;
+  // console.log(...options_req)
+  if(options_req){
+    console.log([...options_req])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <select>{options_req && options_req.map((elm)=>{
+          console.log(elm);
+          return elm;
+        })}</select>
+      </form>
     </div>
   );
 }
---
+++ src/api/api.js
@@ -0,0 +1,4 @@
+
+
+const supportedLanguages = `https://deep-translate1.p.rapidapi.com/language/translate/v2/languages`
+export { supportedLanguages }
