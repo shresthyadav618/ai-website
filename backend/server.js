6efa043f8f3e850b3d691a361deba514dc0664ff
shresthyadav618 <manushresth.official24669@gmail.com>
2023-08-19 23:30:50 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,14 @@ const cors = require('cors');
 const OpenAI = require('openai');
 require('dotenv').config();
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://manuai.onrender.com');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+
 
 app.listen('4000',()=>{
     console.log('listening to the server 4000');
